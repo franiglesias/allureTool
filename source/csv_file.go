710e64b03e9dcad04789eaf039fa3160e54b7f30
Fran Iglesias <fran.iglesias@codurance.com>
2022-04-21 19:56:42 +0200

    Equalize code

--- source/csv_file.go
+++ source/csv_file.go
@@ -15,33 +15,30 @@ func NewCsvFile(path string) *CsvFile {
 }
 
 func (csvFile CsvFile) Read() [][]string {
-	f := openFileForReading(csvFile)
+	source, err := os.Open(csvFile.path)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	defer func(f *os.File) {
 		err := f.Close()
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-	}(f)
+	}(source)
 
-	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
+	data, err := csv.NewReader(source).ReadAll()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	return data
 }
 
-func openFileForReading(csvFile CsvFile) *os.File {
-	f, err := os.Open(csvFile.path)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	return f
-}
-
 func (csvFile CsvFile) Write(data [][]string) {
-	destination := createFileForWriting(csvFile)
+	destination, err := os.Create(csvFile.path)
+	if err != nil {
+		fmt.Println("Failed to open file")
+	}
 
 	defer func(destination *os.File) {
 		err := destination.Close()
@@ -53,18 +50,9 @@ func (csvFile CsvFile) Write(data [][]string) {
 	writer := csv.NewWriter(destination)
 	defer writer.Flush()
 
-	for _, line := range data {
-		err := writer.Write(line)
-		if err != nil {
-			fmt.Println("Error writing line to file")
-		}
-	}
-}
-
-func createFileForWriting(csvFile CsvFile) *os.File {
-	destination, err := os.Create(csvFile.path)
+	err = writer.WriteAll(data)
 	if err != nil {
-		fmt.Println("Failed to open file")
+		fmt.Println("Error writing line to file")
 	}
-	return destination
+
 }
