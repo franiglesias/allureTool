33ba0e359ebf50e5c19976455341e4f48104f59f
Fran Iglesias <fran.iglesias@codurance.com>
2022-04-25 16:06:07 +0200

    Don't try to load invisible files with dot prefix

--- allure-tool.go
+++ allure-tool.go
@@ -4,8 +4,10 @@ import (
 	. "allureTool/config"
 	. "allureTool/report"
 	. "allureTool/source"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -33,10 +35,14 @@ func filesInDir(dir string) []string {
 	var fPaths []string
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed: "+dir, err)
 	}
 
 	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			continue
+		}
+		fmt.Println(file.Name())
 		fPaths = append(fPaths, file.Name())
 	}
 
