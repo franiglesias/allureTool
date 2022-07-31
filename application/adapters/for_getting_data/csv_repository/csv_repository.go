8418ff67b36d818fd11def55ad8e9e8207857cdf
Fran Iglesias <franiglesias@mac.com>
2022-07-31 20:17:03 +0200

    Extract constructor function for making test from a row of csv data (allure report)

--- application/adapters/for_getting_data/csv_repository/csv_repository.go
+++ application/adapters/for_getting_data/csv_repository/csv_repository.go
@@ -2,11 +2,7 @@ package csv_repository
 
 import (
 	"allureTool/application/domain"
-	"fmt"
 	"github.com/spf13/afero"
-	"log"
-	"strconv"
-	"strings"
 )
 
 type CSVRepository struct {
@@ -47,40 +43,9 @@ func convertRawDataToTests(data [][]string) []domain.Test {
 			continue
 		}
 
-		test := domain.Test{
-			Epic:    normalizeLabel(datum[0]),
-			Feature: normalizeLabel(datum[1]),
-			Story:   normalizeLabel(datum[2]),
-			Failed:  strToInt(datum[3]),
-			Broken:  strToInt(datum[4]),
-			Passed:  strToInt(datum[5]),
-			Skipped: strToInt(datum[6]),
-			Unknown: strToInt(datum[7]),
-		}
+		test := MakeTestFromRawData(datum)
 
 		foundTests = append(foundTests, test)
 	}
 	return foundTests
-}
-
-func normalizeLabel(label string) string {
-	if len(label) == 0 {
-		return label
-	}
-
-	parts := strings.SplitN(label, ":", 2)
-
-	if len(parts) != 2 {
-		return label
-	}
-
-	return strings.ReplaceAll(parts[0], " ", "") + ":" + parts[1]
-}
-
-func strToInt(data string) int {
-	converted, err := strconv.Atoi(data)
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("Conversion of %s to int failed", data), err)
-	}
-	return converted
 }
